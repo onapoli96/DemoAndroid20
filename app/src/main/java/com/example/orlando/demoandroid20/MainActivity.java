12edc0980ee9d74c375dd86290846e3f9a61ad99
onapoli96 <o.napoli1@studenti.unisa.it>
2019-05-13 13:39:33 +0200

    Ciao gio

--- app/src/main/java/com/example/orlando/demoandroid20/MainActivity.java
+++ app/src/main/java/com/example/orlando/demoandroid20/MainActivity.java
@@ -40,7 +40,7 @@ import java.util.Collection;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, BeaconConsumer,
         RangeNotifier {
-
+/*Ciao gio*/
     MqttHelper mqttHelper;
 
     TextView dataReceived;
